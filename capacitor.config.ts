b86dade8528dad9c2ced6ea50140a4678c6f38cc
Kuldeep rawat <kuldeep.rawat@kellton.com>
2026-02-20 18:29:24 +0530

    Add initial iOS project files and configurations
    - Created LaunchScreen.storyboard for app launch screen.
    - Added Main.storyboard for the main app interface.
    - Initialized Info.plist with essential app configurations.
    - Set up .gitignore for CapApp-SPM to exclude unnecessary files.
    - Created Package.swift for managing Swift Package Manager dependencies.
    - Added README.md for CapApp-SPM with usage instructions.
    - Implemented CapApp-SPM.swift to define app-specific constants.
    - Configured debug.xcconfig for debug settings.
    - Created environment files for production, QA, and development environments.

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.kuldeep.myapp',
+  appName: 'MyApp',
+  webDir: 'dist/hot-sos-app/browser',
+};
+
+export default config;
